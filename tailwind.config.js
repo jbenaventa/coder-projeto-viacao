cae688d06e061a4866ba895047050bed672a1a11
J Benaventa <juan.benaventa@outlook.com>
2024-06-20 16:15:12 -0300

    save 02.062024-1614 - replaced navbar with optimized tailwind css and dropdown menus

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@
 module.exports = {
   content: ["./*.{html,js}","./node_modules/flowbite/**/*.js"],
   theme: {
+    colors: {
+      'marca': {
+        25: '#FAFFFC',
+        50: '#F5FFFA',
+        100: '#EBFFF5',
+        200: '#D7FED9',
+        300: '#A0EE99',
+        400: '#8DDB59',
+        500: '#629E3A',
+        600: '#0E804B',
+        700: '#095F36',
+        800: '#053E21',
+        900: '#001D0C', 
+      }
+    },
+
     extend: {},
   },
   plugins: [
